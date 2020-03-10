cb77ac0e74edccb86760767073bd02dce4e2596f
xiao <wang@g.com>
2020-03-10 14:10:33 +0800

    v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
+declare let cordova: any;
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+	constructor(private platform: Platform) { }
 
+	record() {
+		console.log("record");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoBase.startRecord();
+		}
+	}
+
+	crop() {
+		console.log("crop");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoBase.startCrop();
+		}
+	}
 }
